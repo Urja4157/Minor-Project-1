2049f1a05673eb48fb3a9d872245507f81cf1429
Monika Shakya <monikashakya1234@gmail.com>
2022-01-04 12:10:18 +0545

    adding new project

---
+++ physiotherapy/physiotherapy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace physiotherapy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
